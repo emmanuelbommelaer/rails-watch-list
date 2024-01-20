bb2e2c3174757a05ce18531449e9b969ec200a14
emmanuel bommelaer <emmanuel.bommelaer@gmail.com>
2024-01-20 12:18:47 +0100

    movie seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+require "json"
+
+puts "loading movies from LW proxy"
+url = "https://tmdb.lewagon.com/movie/top_rated"
+data = JSON.load(URI.open(url))["results"]
+
+if data
+  puts "movies loaded"
+end
+
+data.each do |movie|
+  Movie.create(title: movie["original_title"],
+               overview: movie["overview"],
+               poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+               rating: movie["vote_average"])
+end
