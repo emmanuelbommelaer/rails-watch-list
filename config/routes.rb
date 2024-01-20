a4e143e3f293868dc01a8c94520b5363de47ebf4
emmanuel bommelaer <emmanuel.bommelaer@gmail.com>
2024-01-20 14:47:00 +0100

    list actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/lists", to: "lists#index"
+  post "/lists", to: "lists#create"
+  get "/lists/new", to: "lists#new", as: "new_list"
+  get "/lists/:id", to: "lists#show", as: "list"
   # Defines the root path route ("/")
-  # root "articles#index"
+ root to: "lists#index"
 end
